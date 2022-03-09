64192fb407e6c411572f649f770f4b3d384b4b61
Junrey Beduya <jbeduya@gmail.com>
2022-03-09 22:40:54 +0800

    fixed navigation issue when accessing press-release

--- src/routes/press-release/index.svelte
+++ src/routes/press-release/index.svelte
@@ -17,14 +17,23 @@
 <script>
   import ContentList from "$lib/press-release/ContentList.svelte";
   import Featured from "$lib/press-release/Featured.svelte";
+  import { navigation } from "$lib/stores/navigation";
+
+  // navigation manipulation
+  $navigation.forEach((item) => {
+    item.active = false;
+  });
+
+  $navigation[3].url = "/cocktail-culture";
+  $navigation[5].active = true;
+  $navigation[5].url = "/press-release";
 
   let featured = [
     {
       id: 1,
       image: "/images/press/press.png",
       category: "lifestyle",
-      title:
-        "Tanduay Seen to Drive Rum Growth in Arizona",
+      title: "Tanduay Seen to Drive Rum Growth in Arizona",
       date: "Mar 2021",
       author: "businessinsider.com",
       url: "https://markets.businessinsider.com/news/stocks/tanduay-seen-to-drive-rum-growth-in-arizona-as-it-partners-with-hensley-beverage-company-1030242050",
@@ -36,8 +45,7 @@
       id: 2,
       image: "/images/press/press2.jpg",
       category: "Corporate Partnerships",
-      title:
-        "Tanduay to sponsor five NBA teams",
+      title: "Tanduay to sponsor five NBA teams",
       date: "Apr 2021",
       author: "espn.com",
       url: "https://www.espn.com/basketball/story/_/id/31293568/tanduay-now-sponsor-five-nba-teams-deal-phoenix-suns",
@@ -48,8 +56,7 @@
       id: 3,
       image: "/images/press/press3.jpg",
       category: "Awards",
-      title:
-        "The Top Rums",
+      title: "The Top Rums",
       date: "May 2021",
       author: "forbes.com",
       url: "https://www.forbes.com/sites/joemicallef/2021/05/16/the-top-rums-according-to-new-york-international-spirits-competition/?sh=3e9446957f47",
@@ -64,8 +71,7 @@
       author: "drinkhacker.com",
       url: "https://www.drinkhacker.com/2021/05/18/review-tanduay-double-rum/",
       tags: ["USA"],
-      image:
-        "/images/press/pr-header.png",
+      image: "/images/press/pr-header.png",
       excerpt:
         "The Philippines-based Tanduay Rum is out with a new expression called Double Rum. It’s a blend of two rums, a “premium” rum that’s 16 years old and a “traditional” rum that’s 5 years old",
     },
@@ -77,8 +83,7 @@
       author: "thespiritsbusiness.com",
       url: "https://www.thespiritsbusiness.com/2021/06/rum-brand-champion-2021-tanduay/",
       tags: ["USA"],
-      image:
-        "/images/press/tanduay-champion.png",
+      image: "/images/press/tanduay-champion.png",
 
       excerpt:
         "The Covid‐19 crisis created a challenging environment for million‐case‐selling rum brands last year. Only a handful of the world’s top‐selling labels were able to offset international lockdowns and the economic headwinds that came with them.",
@@ -91,8 +96,7 @@
       author: "brandingnerd.ph",
       url: "https://brandingnerd.ph/2021/07/12/tanduay-rising-to-the-worlds-top-spot-among-rum-brands-through-its-distribution-strategy/",
       tags: ["Philippines"],
-      image:
-        "/images/press/tanduay-rising.png",
+      image: "/images/press/tanduay-rising.png",
       excerpt:
         "There are many reasons for the global success of this Filipino brand, including its strong brand identity and heritage, market segmentation, product quality and innovation, and more. However, one of the strongest pillars of this brilliant brand is its distribution strategy.",
     },
@@ -104,8 +108,7 @@
       author: "yahoo.com",
       url: "https://finance.yahoo.com/news/top-us-spirits-buyers-award-005000103.html",
       tags: ["USA"],
-      image:
-        "/images/press/top-us-spirits.png",
+      image: "/images/press/top-us-spirits.png",
       excerpt:
         "There are many reasons for the global success of this Filipino brand, including its strong brand identity and heritage, market segmentation, product quality and innovation, and more. However, one of the strongest pillars of this brilliant brand is its distribution strategy.",
     },
@@ -117,8 +120,7 @@
       author: "luxurylifestyle.com",
       url: "https://luxurylifestyle.com/headlines/tanduay-the-worlds-largest-rum-brand-enters-benelux.html",
       tags: ["USA"],
-      image:
-        "/images/press/largest-rum-brand.png",
+      image: "/images/press/largest-rum-brand.png",
       excerpt:
         "Tanduay, a popular Filipino rum, is the world's best-selling rum for four consecutive years. However, the brand is not as popular in Europe. Undoubtedly, this will soon change, as Tanduay also wants to put the award-winning rum on its map and has chosen Belgium, the Netherlands, and Luxembourg (collectively, Benelux) as the first countries for this European expansion.",
     },
@@ -130,8 +132,7 @@
       author: "yahoo.com",
       url: "https://finance.yahoo.com/news/tanduay-holiday-cocktails-sure-celebrations-094700287.html",
       tags: ["USA"],
-      image:
-        "/images/press/holiday-cocktails.png",
+      image: "/images/press/holiday-cocktails.png",
       excerpt:
         "Get into the holiday spirit with these cocktails infused with Tanduay's most-awarded and best-selling rum products.",
     },
@@ -143,8 +144,7 @@
       author: "manilatimes.net",
       url: "https://www.manilatimes.net/2022/01/20/public-square/tanduay-awarded-brand-of-the-year-for-7-years-at-intl-forum/1829906",
       tags: ["Philippines"],
-      image:
-        "/images/press/tanduay-award.png",
+      image: "/images/press/tanduay-award.png",
       excerpt:
         "LUCIO Tan (LT) Group's Tanduay was named Brand of the Year 2021 to 2022 at the recent World Branding Awards at Kensington Palace.",
     },
@@ -156,8 +156,7 @@
       author: "yahoo.com",
       url: "https://finance.yahoo.com/news/own-tanduay-cocktails-usher-luck-014100737.html",
       tags: ["USA"],
-      image:
-        "/images/press/diy-cocktails.png",
+      image: "/images/press/diy-cocktails.png",
       excerpt:
         "To liven up the celebrations even more, whether people celebrate with family and friends or on a quiet night at home, here are some delish holiday cocktails people can try making on their own. These cocktails are inspired by the multi-awarded lucky spirits Tanduay Asian Rum Silver and Gold.",
     },
@@ -169,8 +168,7 @@
       author: "businessmirror.com.ph",
       url: "https://businessmirror.com.ph/2022/02/14/celebrate-love-with-tanduays-fun-and-festive-cocktails/",
       tags: ["Philippines"],
-      image:
-        "/images/press/celebrate.png",
+      image: "/images/press/celebrate.png",
       excerpt:
         "Once again it’s that time of the year when love is in the air. Whether you’re celebrating the day of romance with your significant other or with your besties, these delightful cocktails are sure to make the occasion more special.",
     },
